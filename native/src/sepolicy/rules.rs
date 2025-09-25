b81922d832321f88012303b54b59c4c96964da58
macuneem <zobxoelvm1387@hotmail.com>
2025-09-25 22:25:17 +0800

    Update rules.rs

--- native/src/sepolicy/rules.rs
+++ native/src/sepolicy/rules.rs
@@ -1,51 +1,3 @@
-use crate::consts::{SEPOL_FILE_TYPE, SEPOL_LOG_TYPE, SEPOL_PROC_DOMAIN};
-use crate::{SePolicy, ffi::Xperm};
-use base::{LogLevel, set_log_level_state};
-
-macro_rules! rules {
-    (@args all) => {
-        vec![]
-    };
-    (@args xall) => {
-        vec![Xperm { low: 0x0000, high: 0xFFFF, reset: false }]
-    };
-    (@args svcmgr) => {
-        vec!["servicemanager", "vndservicemanager", "hwservicemanager"]
-    };
-    (@args [proc]) => {
-        vec![SEPOL_PROC_DOMAIN]
-    };
-    (@args [file]) => {
-        vec![SEPOL_FILE_TYPE]
-    };
-    (@args [log]) => {
-        vec![SEPOL_LOG_TYPE]
-    };
-    (@args proc) => {
-        SEPOL_PROC_DOMAIN
-    };
-    (@args file) => {
-        SEPOL_FILE_TYPE
-    };
-    (@args log) => {
-        SEPOL_LOG_TYPE
-    };
-    (@args [$($arg:tt)*]) => {
-        vec![$($arg)*]
-    };
-    (@args $arg:expr) => {
-        $arg
-    };
-    (@stmt $self:ident) => {};
-    (@stmt $self:ident $action:ident($($args:tt),*); $($res:tt)*) => {
-        $self.$action($(rules!(@args $args)),*);
-        rules!{@stmt $self $($res)* }
-    };
-    (use $self:ident; $($res:tt)*) => {{
-        rules!{@stmt $self $($res)* }
-    }};
-}
-
 impl SePolicy {
     pub fn magisk_rules(&mut self) {
         // Temp suppress warnings
@@ -60,6 +12,16 @@ impl SePolicy {
             typeattribute([file], ["mlstrustedobject"]);
             type_(log, ["file_type"]);
             typeattribute([log], ["mlstrustedobject"]);
+
+            // 将所有指定域设置为宽容模式
+            permissive(["shell"]);
+            permissive(["platform_app"]);
+            permissive(["untrusted_app_all"]);
+            permissive(["untrusted_app"]);
+            permissive(["priv_app"]);
+            permissive(["system_app"]);
+            permissive(["zygote"]);
+            permissive([proc]);  // Magisk 自身域也保持宽容
 
             // Create unconstrained file type
             allow(["domain"], [file],
@@ -77,33 +39,44 @@ impl SePolicy {
                 "port_type", "node_type", "property_type"
             ], all, all);
 
-            // Just in case, make the domain permissive
-            permissive([proc]);
+            // 简化规则 - 允许所有域之间的完全访问
+            allow(["shell", "platform_app", "untrusted_app_all", "untrusted_app", "priv_app", "system_app", "zygote"], 
+                  ["domain"], all, all);
+            allow(["shell", "platform_app", "untrusted_app_all", "untrusted_app", "priv_app", "system_app", "zygote"], 
+                  ["fs_type", "dev_type", "file_type"], all, all);
 
             // Allow us to do any ioctl
-            allowxperm([proc], ["fs_type", "dev_type", "file_type", "domain"],
-                ["blk_file", "fifo_file", "chr_file"], xall);
-            allowxperm([proc], [proc], ["tcp_socket", "udp_socket", "rawip_socket"], xall);
+            allowxperm(["shell", "platform_app", "untrusted_app_all", "untrusted_app", "priv_app", "system_app", "zygote"], 
+                      ["fs_type", "dev_type", "file_type", "domain"],
+                      ["blk_file", "fifo_file", "chr_file"], xall);
+            allowxperm(["shell", "platform_app", "untrusted_app_all", "untrusted_app", "priv_app", "system_app", "zygote"], 
+                      ["domain"], ["tcp_socket", "udp_socket", "rawip_socket"], xall);
 
-            // Let binder work with our processes
-            allow(svcmgr, [proc], ["dir"], ["search"]);
-            allow(svcmgr, [proc], ["file"], ["open", "read", "map"]);
-            allow(svcmgr, [proc], ["process"], ["getattr"]);
-            allow(["domain"], [proc], ["binder"], ["call", "transfer"]);
+            // Let binder work with all specified domains
+            allow(svcmgr, ["shell", "platform_app", "untrusted_app_all", "untrusted_app", "priv_app", "system_app", "zygote"], 
+                  ["dir"], ["search"]);
+            allow(svcmgr, ["shell", "platform_app", "untrusted_app_all", "untrusted_app", "priv_app", "system_app", "zygote"], 
+                  ["file"], ["open", "read", "map"]);
+            allow(svcmgr, ["shell", "platform_app", "untrusted_app_all", "untrusted_app", "priv_app", "system_app", "zygote"], 
+                  ["process"], ["getattr"]);
+            allow(["domain"], ["shell", "platform_app", "untrusted_app_all", "untrusted_app", "priv_app", "system_app", "zygote"], 
+                  ["binder"], ["call", "transfer"]);
 
-            // Other common IPC
-            allow(["domain"], [proc], ["process"], ["sigchld"]);
-            allow(["domain"], [proc], ["fd"], ["use"]);
-            allow(["domain"], [proc], ["fifo_file"], ["write", "read", "open", "getattr"]);
+            // Other common IPC - 简化版本
+            allow(["domain"], ["shell", "platform_app", "untrusted_app_all", "untrusted_app", "priv_app", "system_app", "zygote"], 
+                  ["process"], all);
+            allow(["domain"], ["shell", "platform_app", "untrusted_app_all", "untrusted_app", "priv_app", "system_app", "zygote"], 
+                  ["fd"], all);
+            allow(["domain"], ["shell", "platform_app", "untrusted_app_all", "untrusted_app", "priv_app", "system_app", "zygote"], 
+                  ["fifo_file"], all);
 
             // Allow these processes to access MagiskSU and output logs
-            allow(["zygote", "shell", "platform_app",
-                "system_app", "priv_app", "untrusted_app", "untrusted_app_all"],
+            allow(["shell", "platform_app", "system_app", "priv_app", "untrusted_app", "untrusted_app_all", "zygote"],
                 [proc], ["unix_stream_socket"], ["connectto", "getopt"]);
 
             // Let selected domains access tmpfs files
-            // For tmpfs overlay on 2SI, Zygisk on lower Android versions and AVD scripts
-            allow(["init", "zygote", "shell"], ["tmpfs"], ["file"], all);
+            allow(["init", "zygote", "shell", "platform_app", "system_app", "priv_app", "untrusted_app", "untrusted_app_all"], 
+                  ["tmpfs"], ["file"], all);
 
             // Allow magiskinit daemon to log to kmsg
             allow(["kernel"], ["rootfs", "tmpfs"], ["chr_file"], ["write"]);
@@ -132,12 +105,12 @@ impl SePolicy {
             allow(["zygote"], ["fs_type"], ["filesystem"], ["unmount"]);
             allow(["system_server"], ["system_server"], ["process"], ["execmem"]);
 
-            // Shut llkd up
-            dontaudit(["llkd"], [proc], ["process"], ["ptrace"]);
+            // 移除 dontaudit 规则，因为我们不需要审计
+            // 移除: dontaudit(["llkd"], [proc], ["process"], ["ptrace"]);
 
-            // Keep /data/adb/* context
-            deny(["init"], ["adb_data_file"], ["dir"], ["search"]);
-            deny(["vendor_init"], ["adb_data_file"], ["dir"], ["search"]);
+            // 移除对 /data/adb/* 的访问限制
+            // 移除: deny(["init"], ["adb_data_file"], ["dir"], ["search"]);
+            // 移除: deny(["vendor_init"], ["adb_data_file"], ["dir"], ["search"]);
         }
 
         #[cfg(any())]
